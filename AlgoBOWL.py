ebd8b0523399e7a2eb746bc9be7ae89bab9563d6
sawyerfallen <160781523+sawyerfallen@users.noreply.github.com>
2024-02-23 12:03:45 -0700

    Update AlgoBOWL.py

--- AlgoBOWL.py
+++ AlgoBOWL.py
@@ -7,7 +7,23 @@ input = sys.argv[1]
 lines = []
 with open(input, 'r') as file:
     for line in file:
-        noSlashInLine = line.rstip('\n')
-        lines.append(noSlashInLine)
+        noSlashnLine = line.rstrip('\n')
+        lines.append(noSlashnLine)
 
+#number of courses
+n = int(lines[0].split()[0])
+
+
+#nodes
+course_graph = nx.DiGraph()
+for i in range(n):
+    course_graph.add_node(i+1)
+
+#add edges
+
+#tracking what node were on
+curr_node = 1
+for node in lines[1:]:
+    for i in range(lines[curr_node].split()[0]):
+        course_graph.add_edge(lines[curr_node].split()[i+1], curr_node)
 
